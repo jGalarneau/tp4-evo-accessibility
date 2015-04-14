be04356a13f908042f1b11446aeeb4e6bdc38eb5
GuillaumeSimard <chadguy2@hotmail.com>
2015-04-14 16:31:23 -0400

    mofification contraste, validation html et autre

--- javascript/pageCourante.js
+++ javascript/pageCourante.js
@@ -1,10 +1,15 @@
 function currentPage()
 {
+
+  	var liens;
 	if(document.getElementById('titre').textContent === "Nous joindre"){
 		document.getElementById('Nous_joindre').setAttribute("class", "PrincipalActive");
 	}
 	else{
-
+			document.getElementById(document.getElementById('titre').textContent).setAttribute("class", "PrincipalActive");
 	}
-	document.getElementById(document.getElementById('titre').textContent).setAttribute("class", "PrincipalActive");
+
+		liens = document.getElementsByClassName("PrincipalActive")[0];
+		var l = liens.getElementsByTagName("a")[0];
+		  l.setAttribute("alt", "vous êtes ici");
 }
