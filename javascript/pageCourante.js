ddd7d10a87ca87b408331080aa3a86f0240715ae
Jimmy Galarneau <jimbohabs_41@hotmail.com>
2015-04-13 21:36:31 -0400

    accessibilité horraire

--- javascript/pageCourante.js
+++ javascript/pageCourante.js
@@ -1,4 +1,10 @@
 function currentPage()
 {
+	if(document.getElementById('titre') === "Nous joindre"){
+		document.getElementById('Nous_joindre').setAttribute("class", "PrincipalActive");
+	}
+	else{
+
+	}
 	document.getElementById(document.getElementById('titre').textContent).setAttribute("class", "PrincipalActive");
 }
